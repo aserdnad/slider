4e7e6c2ddf3931e753ec3defde9ed0cb5ffbc5b6
aserdnad <77126046+aserdnad@users.noreply.github.com>
2022-01-20 22:33:30 +0100

    jugando amo programar esto es vida 20/1/22

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,11 +1,25 @@
 import "./Carousel.css";
 import { images } from "../Helpers/CarouselData";
+import { useState } from "react";
 
 const Carousel = () => {
+  const [indice, setIndice] = useState(0);
+
+  const manejarClick = (e) => {
+    if (indice <= 1) {
+      setIndice(indice + 1);
+    } else {
+      setIndice(0);
+    }
+  };
   return (
     <div className="carousel">
-      <div className="carouselInner">
-        <img src={images[0].img} />
+      <div
+        className="carouselInner"
+        onClick={manejarClick}
+        style={{ backgroundImage: `url(${images[indice].img})` }}
+      >
+        {/* <img src={images[indice].img} /> */}
       </div>
     </div>
   );
