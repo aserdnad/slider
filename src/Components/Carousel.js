9e36ecbc9f5f31706e679cffbea1d9a468d2515b
aserdnad <77126046+aserdnad@users.noreply.github.com>
2022-01-22 21:03:22 +0100

    listo haciendo pruebas para que imagen cambie sola

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,25 +1,56 @@
 import "./Carousel.css";
 import { images } from "../Helpers/CarouselData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowFowardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 const Carousel = () => {
   const [indice, setIndice] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const manejarClick = (e) => {
+  const clickLeft = (e) => {
+    if (indice !== 0) {
+      setIndice(indice - 1);
+    } else {
+      setIndice(2);
+    }
+  };
+  const clickRight = (e) => {
     if (indice <= 1) {
       setIndice(indice + 1);
     } else {
       setIndice(0);
     }
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (count % 2 == 0) {
+        clickRight();
+      }
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="carousel">
       <div
         className="carouselInner"
-        onClick={manejarClick}
         style={{ backgroundImage: `url(${images[indice].img})` }}
       >
-        {/* <img src={images[indice].img} /> */}
+        <div className="left" onClick={clickLeft}>
+          <ArrowBackIosIcon />
+        </div>
+        <div className="center">
+          <h1>{images[indice].title}</h1>
+          <p>{images[indice].subtitle}</p>
+          <h2>{count}</h2>
+        </div>
+        <div className="right" onClick={clickRight}>
+          <ArrowFowardIosIcon />
+        </div>
       </div>
     </div>
   );
